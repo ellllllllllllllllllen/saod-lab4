f89af83e31322172f9dfefcf87bc9923b8a5a007
Lena <ellllllllllllllllllen@mail.ru>
2019-12-13 19:48:21 +0300

    first commit

---
+++ labb4/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labb4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество элементов массива: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine("Кол-во эл-тов массива: "+ number);
+            int[] array = new int[number];
+            Random rand = new Random();
+            for (int i = 1; i < number; i++)
+                array[i] = rand.Next(-100, 100);
+            Console.WriteLine("Исходный массив: ");
+            foreach (int item in array)
+            {
+                Console.Write(item + " ");
+            }
+
+           
+
+            int count = 0;
+            for (int i = 1; i < number; i++)
+                for (int j = i + 1; j < number; j++)
+                    if (array[i] > array[j])
+                    {
+                        int temp2 = array[i];
+                        array[i] = array[j];
+                        array[j] = temp2;
+                        count++;
+                    }
+            Console.WriteLine("\nОтсортированный массив: ");
+            foreach (int item in array)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine("\nКоличество операций перестановки: {0}", count);
+
+
+            int count2 = 0;
+            int temp = array.Max();
+            for (int i = 0; i < number; i++)
+            {
+                count2++;
+                if (temp == array[i])
+                {
+                    Console.WriteLine("Наибольший элемент массива: " + temp);
+                    Console.WriteLine("Количество операций сравнения в линейном поиске: " + count2);
+                }
+
+            }
+            int searchValue = array.Max();
+            int count3 = 0;
+            int low = 0;
+            int high = number;
+            int mid;
+            while (low <= high)
+            {
+                mid = (low + high) / 2;
+                if (searchValue == array[mid])
+                {
+                    Console.WriteLine("Наибольший элемент массива: " + searchValue);
+                    count3++;
+                    break;
+                }
+                else if (searchValue > array[mid])
+                {
+                    low = mid + 1;
+                    count3++;
+                }
+                else
+                {
+                    high = mid - 1;
+                    count3++;
+                }
+            }
+            Console.WriteLine("Количество операций сравнения в бинарном поиске: " + count3);
+        }
+    }
+}
---
+++ labb4/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("labb4")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("labb4")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("c2f4b0f2-41db-45d7-a91d-2f3f212875f4")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
